cda0652a24693b79937eeea2342452849bdcdb9d
baegteun <baegteun@gmail.com>
2022-06-08 00:16:51 +0900

    :sparkles: :: Realm, Results Extension

---
+++ Projects/Service/LocalDataSource/Sources/Utils/Extensions/Realm+Ext.swift
@@ -0,0 +1,11 @@
+import RealmSwift
+
+extension Realm {
+    func safeWrite(_ block: (() throws -> Void)) throws {
+        if isInWriteTransaction {
+            try block()
+        } else {
+            try write(block)
+        }
+    }
+}
